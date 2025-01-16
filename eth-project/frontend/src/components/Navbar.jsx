b8c18030bd676b3fbcbfd67746d7d6a888150434
dennislee928 <pcleegood@gmail.com>
2025-01-16 15:34:37 +0800

    :0

--- eth-project/frontend/src/components/Navbar.jsx
+++ eth-project/frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./component-css/navbar.css";
+import "../styles/navbar.css";
 import { useTranslation } from "react-i18next";
 
 const Navbar = () => {

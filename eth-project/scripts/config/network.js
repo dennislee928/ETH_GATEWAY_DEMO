326902aa5327d547bf9325ece1de5b12915f6716
dennislee928 <pcleegood@gmail.com>
2025-01-16 12:26:43 +0800

    Update network.js

--- eth-project/scripts/config/network.js
+++ eth-project/scripts/config/network.js
@@ -6,6 +6,16 @@ const networks = {
     wsUrl: process.env.QUICKNODE_WS_URL,
     chainId: 11155111,
     explorer: "https://sepolia.etherscan.io",
+    contracts: {
+      simpleStorage: {
+        address: process.env.CONTRACT_ADDRESS,
+        abi: [
+          "function setValue(uint256 newValue) public",
+          "function getValue() public view returns (uint256)",
+          "event ValueChanged(uint256 newValue)",
+        ],
+      },
+    },
   },
 };
 
